63d506eff95ccabed6895da88512f8cde0c7f413
Anatoli Scameroni <anatoli.scameroni@avanade.com>
2026-05-20 20:49:34 +0200

    Add BusinessLogic and Interfaces di function app: ReleaseExpiredCartKeysTimer

---
+++ CustomAppDigitalProducts/FunctionApp.sc_DigitalProducts/BusinessLogic/ReleaseExpiredCartKeysTimerBL.cs
@@ -0,0 +1,22 @@
+﻿using FunctionApp.sc_DigitalProducts.Entities;
+using FunctionApp.sc_DigitalProducts.Interfaces;
+using Microsoft.Extensions.Logging;
+
+
+namespace FunctionApp.sc_DigitalProducts.BusinessLogic
+{
+    public class ReleaseExpiredCartKeysTimerBL : IReleaseExpiredCartKeysTimerBL
+    {
+        private readonly ILogger _logger;
+
+        public ReleaseExpiredCartKeysTimerBL(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public List<DigitalProduct> GetDigitalProductList(int IntTopQueryDigitalProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomAppDigitalProducts/FunctionApp.sc_DigitalProducts/Interfaces/IReleaseExpiredCartKeysTimerBL.cs
@@ -0,0 +1,16 @@
+﻿using FunctionApp.sc_DigitalProducts.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp.sc_DigitalProducts.Interfaces
+{
+    public interface IReleaseExpiredCartKeysTimerBL
+    {
+        public List<DigitalProduct> GetDigitalProductList(int IntTopQueryDigitalProduct);
+    }
+}
+ 
